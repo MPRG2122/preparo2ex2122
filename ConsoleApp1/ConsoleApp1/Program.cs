b6bd5079d27e7511ae9ea80a23015dd8c8067c2e
MPRG2122 <oficinacontabilidadalicante@gmail.com>
2022-03-14 19:11:39 +0100

    subo codigo

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mayor = 0;
+            int menor = 0;
+            Console.WriteLine("Introduce un número:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce otro número:");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("-------");
+
+            if (num1 > num2)
+            {
+                mayor = num2;
+                menor = num1;
+            }
+            else
+            {
+                mayor = num1;
+                menor = num2;
+            }
+            Console.WriteLine("El mayor es:" + mayor + "\nEl menor es: " + menor);
+            Console.ReadKey();
+        }
+    }
+}
